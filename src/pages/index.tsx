298b30c7dec85d90fe4861d1a1aea55fa3da9e46
Lucas Serafim <lucas.serafin@protonmail.ch>
2021-02-27 22:01:23 -0300

    Novas atualizaçoes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import {ExperienceBar} from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import style from '../styles/pages/Home.module.css';
+
+import Head from 'next/head';
 export default function Home() {
   return (
     <div className={style.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar/>
 
       <section >
         <div>
           <Profile/>
           <CompletedChallenges/>
+          <Countdown/>
         </div>
         <div>
 
